808fbc8d44e74f0a9b1eb3bff4019041dc0a4aee
Chris <chris@cookseys.org>
2018-08-03 11:53:16 -0400

    dice image changes on click

--- app.js
+++ app.js
@@ -15,17 +15,17 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x)
-
 document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
 
 	var dice = Math.floor(Math.random() * 6) + 1;
 
-	
+	var diceDOM = document.querySelector('.dice'); 
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+
+})
 
 
 
@@ -33,4 +33,11 @@ document.querySelector('.dice').style.display = 'none';
 
 
 
+
+
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// var x = document.querySelector('#score-0').textContent;
 
