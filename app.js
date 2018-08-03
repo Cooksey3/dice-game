2f1908ae34c88def6356cfbaf2b314d04a3a189c
Chris <chris@cookseys.org>
2018-08-03 11:14:44 -0400

    Working on dice rolling and adding features

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = 6;
+dice = Math.floor(Math.random() * 6) + 1;
 
-
+document.querySelector('#current-' + activePlayer).textContent = dice;
 
 
 
