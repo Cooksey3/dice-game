d8d4c7afe0e0da3c3241373e30ffb56349b881ad
Chris <chris@cookseys.org>
2018-08-03 12:49:04 -0400

    added initialization function for game

--- app.js
+++ app.js
@@ -11,16 +11,21 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+function init() {
+	scores = [0, 0];
+	activePlayer = 0;
+	roundScore = 0;	
+	
+	document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('.dice').style.display = 'none';
+	document.getElementById('score-0').textContent = '0'
+	document.getElementById('score-1').textContent = '0'
+	document.getElementById('current-0').textContent = '0'
+	document.getElementById('current-1').textContent = '0'
+}
 
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
+init();
+
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
 
@@ -67,6 +72,8 @@ function nextPlayer() {
 
 	document.querySelector('.dice').style.display = 'none';
 }
+
+document.querySelector('.btn-new').addEventListener('click', init);
 
 
 
